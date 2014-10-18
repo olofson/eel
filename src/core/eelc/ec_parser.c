olofson/eel

commit c4a263844d3ec24158e82d06ec36364dc7af48f5

Trailing commas in lists now result in compiler warnings
See issue #50. Considering actually throwing errors instead.

src/core/eelc/ec_parser.c
@@ -1009,7 +1009,11 @@ static int listof(EEL_state *es, EEL_mlist *al,
 		switch(what(es, al, xopt))
 		{
 		  case TK_WRONG:
-			return first ? TK(WRONG) : TK2("LISTOF <?>", TK_EXPLIST);
+			if(!first)
+				eel_cwarning(es, "Trailing comma in %s list.",
+						whatn);
+			return first ? TK(WRONG) :
+					TK2("LISTOF <?>", TK_EXPLIST);
 		  case TK_VOID:
 			if(first && (es->token != ','))
 				return TK(VOID);
@@ -1893,6 +1897,7 @@ static int vardecl(EEL_state *es, EEL_mlist *al)
 static int tablector(EEL_state *es, EEL_mlist *al)
 {
 	int r, lastcount;
+	int first = 1;
 	EEL_coder *cdr;
 	EEL_mlist *inits;
 
@@ -1916,7 +1921,12 @@ static int tablector(EEL_state *es, EEL_mlist *al)
 	{
 		int func_by_name = 0;
 		if(']' == es->token)
+		{
+			if(!first)
+				eel_cwarning(es, "Trailing comma in table "
+						" constructor.");
 			break;
+		}
 		lastcount = inits->length;
 		if('(' == es->token)
 		{
@@ -1974,6 +1984,7 @@ static int tablector(EEL_state *es, EEL_mlist *al)
 				break;
 			eel_lex(es, 0);
 		}
+		first = 0;
 	}
 
 	/* Closing brace */

commit 7d4c9f56c3692ce2d940400e75946c744021b1b0

Fixed bogus trailing comma warnings in table constructors
Fixed a logic bug introduced by previous commit c4a263844, triggering
the trailing comma warning if a table initializer ended with a named
function.

src/core/eelc/ec_parser.c
@@ -1897,7 +1897,7 @@ static int vardecl(EEL_state *es, EEL_mlist *al)
 static int tablector(EEL_state *es, EEL_mlist *al)
 {
 	int r, lastcount;
-	int first = 1;
+	int comma = 0;
 	EEL_coder *cdr;
 	EEL_mlist *inits;
 
@@ -1921,12 +1921,7 @@ static int tablector(EEL_state *es, EEL_mlist *al)
 	{
 		int func_by_name = 0;
 		if(']' == es->token)
-		{
-			if(!first)
-				eel_cwarning(es, "Trailing comma in table "
-						" constructor.");
 			break;
-		}
 		lastcount = inits->length;
 		if('(' == es->token)
 		{
@@ -1972,20 +1967,27 @@ static int tablector(EEL_state *es, EEL_mlist *al)
 					(inits->length - lastcount != 2))
 				eel_cerror(es, "Expected value expression!");
 		}
+		comma = 0;
 		if(func_by_name)
 		{
 			/* Coma is optional after "function by name"! */
 			if(',' == es->token)
+			{
+				comma = 1;
 				eel_lex(es, 0);
+			}
 		}
 		else
 		{
 			if(',' != es->token)
 				break;
 			eel_lex(es, 0);
+			comma = 1;
 		}
-		first = 0;
 	}
+
+	if(comma)
+		eel_cwarning(es, "Trailing comma in table constructor.");
 
 	/* Closing brace */
 	expect(es, ']', NULL);