5d9291d3dcad6cfb1af02e0ec036bec55250ab43
David Olofson <david@olofson.net>
2014-11-05 02:34:56 +0100

    Updated the Audiality 2 binding to track Audiality 2 master
    Apart from minor details, this is mostly about covering the asynchronous
    stream API, and adding various test scripts for it.
    
    This is for #8, but covers only part of it!

--- src/eelium/audiality2/eel_audiality2.c
+++ src/eelium/audiality2/eel_audiality2.c
@@ -22,6 +22,7 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
 #include "eel_audiality2.h"
 
 
@@ -42,6 +43,55 @@ static int ea2_error_base;
 static inline EEL_xno ea2_translate_error(A2_errors a2e)
 {
 	return (EEL_xno)a2e + ea2_error_base;
+}
+
+
+static inline EEL_xno get_raw_data_o(EEL_object *o, A2_sampleformats *fmt,
+		void **data, unsigned *size, unsigned *itemsize)
+{
+	*size = eel_length(o);
+	*data = eel_rawdata(o);
+	if(!*data)
+		return EEL_XWRONGTYPE;
+	switch((EEL_classes)o->type)
+	{
+	  case EEL_CSTRING:
+	  case EEL_CDSTRING:
+	  case EEL_CVECTOR_S8:
+		*fmt = A2_I8;
+		if(itemsize)
+			*itemsize = 1;
+		return 0;
+	  case EEL_CVECTOR_S16:
+		*fmt = A2_I16;
+		*size *= 2;
+		if(itemsize)
+			*itemsize = 2;
+		return 0;
+	  case EEL_CVECTOR_S32:
+		*fmt = A2_I24;
+		*size *= 4;
+		if(itemsize)
+			*itemsize = 4;
+		return 0;
+	  case EEL_CVECTOR_F:
+		*fmt = A2_F32;
+		*size *= 4;
+		if(itemsize)
+			*itemsize = 4;
+		return 0;
+	  default:
+		return EEL_XWRONGTYPE;
+	}
+}
+
+/* Check type, calculate size and get raw data pointer */
+static EEL_xno get_raw_data(EEL_value *v, A2_sampleformats *fmt,
+		void **data, unsigned *size, unsigned *itemsize)
+{
+	if(!EEL_IS_OBJREF(v->type))
+		return EEL_XNEEDOBJECT;
+	return get_raw_data_o(v->objref.v, fmt, data, size, itemsize);
 }
 
 
@@ -497,6 +547,8 @@ static EEL_xno ea2_Start(EEL_vm *vm)
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
+	if(vm->argc - 3 > A2_MAXARGS)
+		return EEL_XMANYARGS;
 	for(i = 0; i < vm->argc - 3; ++i)
 		a[i] = eel_v2d(args + 3 + i) * 65536.0f;
 	eel_l2v(vm->heap + vm->resv, a2_Starta(ea2s->state, eel_v2l(args + 1),
@@ -513,6 +565,8 @@ static EEL_xno ea2_Play(EEL_vm *vm)
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
+	if(vm->argc - 3 > A2_MAXARGS)
+		return EEL_XMANYARGS;
 	for(i = 0; i < vm->argc - 3; ++i)
 		a[i] = eel_v2d(args + 3 + i) * 65536.0f;
 	eel_l2v(vm->heap + vm->resv, a2_Playa(ea2s->state, eel_v2l(args + 1),
@@ -529,6 +583,8 @@ static EEL_xno ea2_Send(EEL_vm *vm)
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
+	if(vm->argc - 3 > A2_MAXARGS)
+		return EEL_XMANYARGS;
 	for(i = 0; i < vm->argc - 3; ++i)
 		a[i] = eel_v2d(args + 3 + i) * 65536.0f;
 	eel_l2v(vm->heap + vm->resv, a2_Senda(ea2s->state, eel_v2l(args + 1),
@@ -545,6 +601,8 @@ static EEL_xno ea2_SendSub(EEL_vm *vm)
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
+	if(vm->argc - 3 > A2_MAXARGS)
+		return EEL_XMANYARGS;
 	for(i = 0; i < vm->argc - 3; ++i)
 		a[i] = eel_v2d(args + 3 + i) * 65536.0f;
 	eel_l2v(vm->heap + vm->resv, a2_SendSuba(ea2s->state, eel_v2l(args + 1),
@@ -578,54 +636,411 @@ static EEL_xno ea2_KillSub(EEL_vm *vm)
 
 
 /*---------------------------------------------------------
-	Waveform management
+	Streams
 ---------------------------------------------------------*/
 
-/* WaveUpload(state, wavetype, period, flags, data) */
-static EEL_xno ea2_WaveUpload(EEL_vm *vm)
+/* function OpenStream(state, handle)[channel, size, flags] */
+static EEL_xno ea2_OpenStream(EEL_vm *vm)
 {
 	EEL_value *args = vm->heap + vm->argv;
 	EA2_state *ea2s;
-	EEL_value *d = args + 4;
+	int channel = 0;
+	int size = 0;
+	unsigned flags = 0;
+	A2_handle h;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	switch(vm->argc)
+	{
+	  case 5:
+		flags = eel_v2l(args + 4);
+	  case 4:
+		size = eel_v2l(args + 3);
+	  case 3:
+		channel = eel_v2l(args + 2);
+	}
+	h = a2_OpenStream(ea2s->state, eel_v2l(args + 1),
+			channel, size, flags);
+	if(h < 0)
+		return ea2_translate_error(-h);
+	eel_l2v(vm->heap + vm->resv, h);
+	return 0;
+}
+
+/* function OpenSink(state, voice, channel, size)[flags] */
+static EEL_xno ea2_OpenSink(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	A2_handle h;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	h = a2_OpenSink(ea2s->state, eel_v2l(args + 1), eel_v2l(args + 2),
+			eel_v2l(args + 3),
+			vm->argc >= 5 ? eel_v2l(args + 4) : 0);
+	if(h < 0)
+		return ea2_translate_error(-h);
+	eel_l2v(vm->heap + vm->resv, h);
+	return 0;
+}
+
+/* function OpenSource(state, voice, channel, size)[flags] */
+static EEL_xno ea2_OpenSource(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	A2_handle h;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	h = a2_OpenSource(ea2s->state, eel_v2l(args + 1), eel_v2l(args + 2),
+			eel_v2l(args + 3),
+			vm->argc >= 5 ? eel_v2l(args + 4) : 0);
+	if(h < 0)
+		return ea2_translate_error(-h);
+	eel_l2v(vm->heap + vm->resv, h);
+	return 0;
+}
+
+/* procedure SetPosition(state, handle, offset) */
+static EEL_xno ea2_SetPosition(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	A2_errors res;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	if((res = a2_SetPosition(ea2s->state, eel_v2l(args + 1),
+			eel_v2l(args + 2))))
+		return ea2_translate_error(res);
+	return 0;
+}
+
+/* function GetPosition(state, handle) */
+static EEL_xno ea2_GetPosition(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	int res;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	if((res = a2_GetPosition(ea2s->state, eel_v2l(args + 1))) < 0)
+		return ea2_translate_error(-res);
+	eel_l2v(vm->heap + vm->resv, res);
+	return 0;
+}
+
+/* function Available(state, handle) */
+static EEL_xno ea2_Available(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	int res;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	if((res = a2_Available(ea2s->state, eel_v2l(args + 1))) < 0)
+		return ea2_translate_error(-res);
+	eel_l2v(vm->heap + vm->resv, res);
+	return 0;
+}
+
+/* function Space(state, handle) */
+static EEL_xno ea2_Space(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	int res;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	if((res = a2_Space(ea2s->state, eel_v2l(args + 1))) < 0)
+		return ea2_translate_error(-res);
+	eel_l2v(vm->heap + vm->resv, res);
+	return 0;
+}
+
+/*
+ * function Read(state, handle, frames)[target, offset]
+ *
+ *	Reads the specified number of audio sample frames from stream 'handle'
+ *	into an indexable container that is either provided by the caller, or
+ *	created by this call.
+ *
+ *	If 'target' is a vector type ID, the sample frames read are returned
+ *	as a vector of that type, or, if 'target' is not specified, the
+ *	closest matching the native type of the data.
+ *
+ *	If 'target' is a supported indexable object, the samples are converted
+ *	and written into that object. 'target' is grown as needed, but will not
+ *	be truncated if its size is greater than 'frames'.
+ *
+ *	If 'offset' is specified, it specifies the index of 'target' where the
+ *	first sample will be written.
+ *
+ *	Returns 'target', or if 'target' was not specified, or a type ID, a new
+ *	object containing the data read.
+ *
+ *	This call is non-blocking, and will throw an exception if the 'target'
+ *	stream cannot instantly receive the amount of data to write.
+ */
+static EEL_xno ea2_Read(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	EEL_object *o;
+	A2_errors res;
 	A2_sampleformats fmt;
-	const void *data;
+	void *data;
+	unsigned size, isize;
+	unsigned offset = vm->argc >= 5 ? eel_v2l(args + 4) : 0;
+	unsigned frames = eel_v2l(args + 2);
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+
+	/* Create or get target object */
+	if(vm->argc < 4)
+	{
+		/*
+		 * Create object of type matching native format
+		 * FIXME: Hardwired to vector_f for now...
+		 */
+		if(!(o = eel_new_indexable(vm, EEL_CVECTOR_F,
+				offset + frames)))
+			return EEL_XCONSTRUCTOR;
+	}
+	else if(args[3].type == EEL_TTYPEID)
+	{
+		/* Create result object of specified type */
+		if(!(o = eel_new_indexable(vm, args[3].integer.v,
+				offset + frames)))
+			return EEL_XCONSTRUCTOR;
+	}
+	else
+	{
+		/* Write to existing object! */
+		if(!EEL_IS_OBJREF(args[3].type))
+			return EEL_XNEEDOBJECT;
+		o = args[3].objref.v;
+		eel_own(o);	/* We're going to return this as result! */
+
+		/* Make sure the target object is large enough! */
+		if(eel_length(o) < offset + frames)
+		{
+			EEL_xno x;
+			EEL_value v;
+			eel_l2v(&v, 0);
+			if((x = eel_setlindex(o, offset + frames - 1, &v)))
+				return x;
+		}
+	}
+
+	if((res = get_raw_data_o(o, &fmt, &data, &size, &isize)))
+		return res;
+	data = (char *)data + isize * offset;
+	size = isize * frames;
+
+	/* Read! */
+	if((res = a2_Read(ea2s->state, eel_v2l(args + 1), fmt, data, size)))
+		return ea2_translate_error(res);
+
+	eel_o2v(vm->heap + vm->resv, o);
+	return 0;
+}
+
+/*
+ * procedure Write(state, handle, frames, source)[offset]
+ *
+ *	Writes the specified number of audio sample frames from the indexable
+ *	container 'source' to stream 'handle'. 'source' needs to be of a signed
+ *	integer or floating point vector type.
+ *
+ *	If 'offset' is specified, it specifies the index of 'source' where the
+ *	first sample will be read from.
+ *
+ *	This call is non-blocking, and will throw an exception if the 'source'
+ *	stream cannot instantly deliver the amount of data requested.
+ */
+static EEL_xno ea2_Write(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	EEL_object *o;
+	A2_errors res;
+	A2_sampleformats fmt;
+	void *data;
+	unsigned size, isize;
+	unsigned offset = vm->argc >= 5 ? eel_v2l(args + 4) : 0;
+	unsigned frames = eel_v2l(args + 2);
+
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+
+	/* Get and check source object */
+	if(!EEL_IS_OBJREF(args[3].type))
+		return EEL_XNEEDOBJECT;
+	o = args[3].objref.v;
+	if(eel_length(o) < offset + frames)
+		return EEL_XHIGHINDEX;
+	if((res = get_raw_data_o(o, &fmt, &data, &size, &isize)))
+		return res;
+	data = (char *)data + isize * offset;
+	size = isize * frames;
+
+	/* Write! */
+	if((res = a2_Write(ea2s->state, eel_v2l(args + 1), fmt, data, size)))
+		return ea2_translate_error(res);
+	return 0;
+}
+
+/* procedure Flush(state, handle) */
+static EEL_xno ea2_Flush(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	A2_errors res;
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+	if((res = a2_Flush(ea2s->state, eel_v2l(args + 1))))
+		return ea2_translate_error(res);
+	return 0;
+}
+
+
+/*---------------------------------------------------------
+	Waveform management
+---------------------------------------------------------*/
+
+/* UploadWave(state, wavetype, period, flags, data) */
+static EEL_xno ea2_UploadWave(EEL_vm *vm)
+{
+	EEL_xno res;
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	A2_sampleformats fmt;
+	void *data;
 	unsigned size;
 	A2_handle h;
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
-	size = eel_length(d->objref.v);
-	data = eel_rawdata(d->objref.v);
-	if(!data)
-		return EEL_XWRONGTYPE;
-	switch((EEL_classes)EEL_TYPE(d))
+	if((res = get_raw_data(args + 4, &fmt, &data, &size, NULL)))
+		return res;
+	h = a2_UploadWave(ea2s->state, eel_v2l(args + 1), eel_v2l(args + 2),
+			eel_v2l(args + 3), fmt, data, size);
+	if(h < 0)
+		return ea2_translate_error(-h);
+	eel_l2v(vm->heap + vm->resv, h);
+	return 0;
+}
+
+
+static EEL_xno ea2_parse_properties(EEL_vm *vm, EEL_object *a, A2_property **p)
+{
+	EEL_xno res;
+	int i, len;
+	if(!(len = eel_length(a)))
 	{
-	  case EEL_CSTRING:
-	  case EEL_CDSTRING:
-		fmt = A2_I8;
-		data = eel_v2s(d);
+		*p = NULL;
+		return 0;
+	}
+	if(len & 1)
+		return EEL_XNEEDEVEN;
+	len /= 2;
+	if(!(*p = (A2_property *)malloc(sizeof(A2_property) * len)))
+		return EEL_XMEMORY;
+	for(i = 0; i < len; ++i)
+	{
+		EEL_value v;
+		if((res = eel_getlindex(a, i * 2, &v)))
+			break;
+		if(EEL_TYPE(&v) != EEL_TINTEGER)
+		{
+			eel_v_disown(&v);
+			break;
+		}
+		(*p)[i].property = eel_v2l(&v);
+		if((res = eel_getlindex(a, i * 2 + 1, &v)))
+			break;
+		if((EEL_TYPE(&v) != EEL_TINTEGER) &&
+				(EEL_TYPE(&v) != EEL_TREAL))
+		{
+			eel_v_disown(&v);
+			break;
+		}
+		(*p)[i].property = eel_v2l(&v);
+	}
+	if(res)
+	{
+		free(*p);
+		return res;
+	}
+	return 0;
+}
+
+/*
+ * RenderWave(state, wavetype, period, flags, samplerate, length)<args>
+ * where <args> is an optional array of properties (<property, value> pairs)
+ * followed by a program handle and zero or more arguments that will be passed
+ * to the program.
+ */
+static EEL_xno ea2_RenderWave(EEL_vm *vm)
+{
+	EEL_value *args = vm->heap + vm->argv;
+	EA2_state *ea2s;
+	A2_handle h, progh;
+	A2_property *props = NULL;
+	int i, a[A2_MAXARGS], fargs;
+
+	/* State */
+	if(EEL_TYPE(args) != a2_md.state_cid)
+		return EEL_XWRONGTYPE;
+	ea2s = o2EA2_state(args->objref.v);
+
+	/* Properties and/or program handle */
+	switch((EEL_classes)EEL_TYPE(args + 6))
+	{
+	  case EEL_TINTEGER:
+		progh = eel_v2l(args + 6);
+		fargs = 7;
 		break;
-	  case EEL_CVECTOR_S8:
-		fmt = A2_I8;
-		size = eel_length(d->objref.v);
+	  case EEL_CARRAY:
+	  {
+		EEL_xno res;
+		if(vm->argc < 8)
+			return EEL_XFEWARGS;
+		if((res = ea2_parse_properties(vm, args[6].objref.v, &props)))
+			return res;
+		fargs = 8;
+		progh = eel_v2l(args + 7);
 		break;
-	  case EEL_CVECTOR_S16:
-		fmt = A2_I16;
-		size *= 2;
-		break;
-	  case EEL_CVECTOR_S32:
-		fmt = A2_I32;
-		size *= 4;
-		break;
-	  case EEL_CVECTOR_F:
-		fmt = A2_F32;
-		size *= 4;
-		break;
+	  }
 	  default:
 		return EEL_XWRONGTYPE;
 	}
-	h = a2_WaveUpload(ea2s->state, eel_v2l(args + 1), eel_v2l(args + 2),
-			eel_v2l(args + 3), fmt, data, size);
+
+	/* Program arguments */
+	if(vm->argc - fargs > A2_MAXARGS)
+	{
+		free(props);
+		return EEL_XMANYARGS;
+	}
+	for(i = 0; i < vm->argc - fargs; ++i)
+		a[i] = eel_v2d(args + fargs + i) * 65536.0f;
+
+	h = a2_RenderWave(ea2s->state,
+			eel_v2l(args + 1), eel_v2l(args + 2), /* wt, period */
+			eel_v2l(args + 3), eel_v2l(args + 4), /* flags, fs */
+			eel_v2l(args + 5), props, /* length, properties */
+			progh, vm->argc - fargs, a); /* program, arguments */
+	free(props);
 	if(h < 0)
 		return ea2_translate_error(-h);
 	eel_l2v(vm->heap + vm->resv, h);
@@ -670,6 +1085,10 @@ static EEL_xno ea2_GetProperty(EEL_vm *vm)
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
+	if(EEL_TYPE(args + 1) != EEL_TINTEGER)
+		return EEL_XWRONGTYPE;
+	if(EEL_TYPE(args + 2) != EEL_TINTEGER)
+		return EEL_XWRONGTYPE;
 	if((ae = a2_GetProperty(ea2s->state, eel_v2l(args + 1),
 			eel_v2l(args + 2), &v)))
 		return ea2_translate_error(ae);
@@ -677,7 +1096,6 @@ static EEL_xno ea2_GetProperty(EEL_vm *vm)
 	return 0;
 }
 
-/* GetProperty(state, handle, property) */
 /* SetProperty(state, handle, property, value) */
 static EEL_xno ea2_SetProperty(EEL_vm *vm)
 {
@@ -687,6 +1105,10 @@ static EEL_xno ea2_SetProperty(EEL_vm *vm)
 	if(EEL_TYPE(args) != a2_md.state_cid)
 		return EEL_XWRONGTYPE;
 	ea2s = o2EA2_state(args->objref.v);
+	if(EEL_TYPE(args + 1) != EEL_TINTEGER)
+		return EEL_XWRONGTYPE;
+	if(EEL_TYPE(args + 2) != EEL_TINTEGER)
+		return EEL_XWRONGTYPE;
 	if((ae = a2_SetProperty(ea2s->state, eel_v2l(args + 1),
 			eel_v2l(args + 2), eel_v2l(args + 3))))
 		return ea2_translate_error(ae);
@@ -806,25 +1228,47 @@ static const EEL_lconstexp eel_a2_constants[] =
 	{"CLEAR",		A2_CLEAR	},
 	{"UNPREPARED",		A2_UNPREPARED	},
 
-	/* Properties: Global settings and profiling information */
-	{"PSAMPLERATE",		A2_PSAMPLERATE	},
-	{"PBUFFER",		A2_PBUFFER	},
-	{"PACTIVEVOICES",	A2_PACTIVEVOICES},
-	{"PFREEVOICES",		A2_PFREEVOICES	},
-	{"PTOTALVOICES",	A2_PTOTALVOICES	},
-	{"PCPULOADAVG",		A2_PCPULOADAVG	},
-	{"PCPULOADMAX",		A2_PCPULOADMAX	},
-	{"PCPUTIMEAVG",		A2_PCPUTIMEAVG	},
-	{"PCPUTIMEMAX",		A2_PCPUTIMEMAX	},
-	{"PINSTRUCTIONS",	A2_PINSTRUCTIONS},
-	{"PEXPORTALL",		A2_PEXPORTALL	},
+	/* Sample formats for wave uploading, stream I/O etc */
+	{"I8",			A2_I8		},
+	{"I16",			A2_I16		},
+	{"I24",			A2_I24		},
+	{"I32",			A2_I32		},
+	{"F32",			A2_F32		},
 
-	/* Properties: General properties */
-	{"PCHANNELS",		A2_PCHANNELS	},
-	{"PFLAGS",		A2_PFLAGS	},
+	/* Properties: General */
+	{"PCHANNELS",		A2_PCHANNELS		},
+	{"PFLAGS",		A2_PFLAGS		},
+	{"PREFCOUNT",		A2_PREFCOUNT		},
 
-	/* Properties: Wave properties */
-	{"PLOOPED",		A2_PLOOPED	},
+	{"PSIZE",		A2_PSIZE		},
+	{"PPOSITION",		A2_PPOSITION		},
+	{"PAVAILABLE",		A2_PAVAILABLE		},
+	{"PSPACE",		A2_PSPACE		},
+
+	/* Properties: Global settings */
+	{"PSAMPLERATE",		A2_PSAMPLERATE		},
+	{"PBUFFER",		A2_PBUFFER		},
+	{"PEXPORTALL",		A2_PEXPORTALL		},
+	{"PTABSIZE",		A2_PTABSIZE		},
+	{"POFFLINEBUFFER",	A2_POFFLINEBUFFER	},
+	{"PSILENCELEVEL",	A2_PSILENCELEVEL	},
+	{"PSILENCEWINDOW",	A2_PSILENCEWINDOW	},
+	{"PSILENCEGRACE",	A2_PSILENCEGRACE	},
+	{"PRANDSEED",		A2_PRANDSEED		},
+	{"PNOISESEED",		A2_PNOISESEED		},
+
+	/* Properties: Profiling information */
+	{"PACTIVEVOICES",	A2_PACTIVEVOICES	},
+	{"PFREEVOICES",		A2_PFREEVOICES		},
+	{"PTOTALVOICES",	A2_PTOTALVOICES		},
+	{"PCPULOADAVG",		A2_PCPULOADAVG		},
+	{"PCPULOADMAX",		A2_PCPULOADMAX		},
+	{"PCPUTIMEAVG",		A2_PCPUTIMEAVG		},
+	{"PCPUTIMEMAX",		A2_PCPUTIMEMAX		},
+	{"PINSTRUCTIONS",	A2_PINSTRUCTIONS	},
+
+	/* Properties: Wave */
+	{"PLOOPED",		A2_PLOOPED		},
 
 	{NULL, 0}
 };
@@ -925,8 +1369,21 @@ EEL_xno eel_audiality2_init(EEL_vm *vm)
 	addfunc(m, t, 1, "Kill", 2, 0, 0, ea2_Kill);
 	addfunc(m, t, 1, "KillSub", 2, 0, 0, ea2_KillSub);
 
+	/* Streams */
+	addfunc(m, t, 1, "OpenStream", 2, 3, 0, ea2_OpenStream);
+	addfunc(m, t, 1, "OpenSink", 4, 1, 0, ea2_OpenSink);
+	addfunc(m, t, 1, "OpenSource", 4, 1, 0, ea2_OpenSource);
+	addfunc(m, t, 0, "SetPosition", 3, 0, 0, ea2_SetPosition);
+	addfunc(m, t, 1, "GetPosition", 2, 0, 0, ea2_GetPosition);
+	addfunc(m, t, 1, "Available", 2, 0, 0, ea2_Available);
+	addfunc(m, t, 1, "Space", 2, 0, 0, ea2_Space);
+	addfunc(m, t, 1, "Read", 3, 2, 0, ea2_Read);
+	addfunc(m, t, 0, "Write", 3, 2, 0, ea2_Write);
+	addfunc(m, t, 0, "Flush", 2, 0, 0, ea2_Flush);
+
 	/* Waveform management (waves.h) */
-	addfunc(m, t, 1, "WaveUpload", 5, 0, 0, ea2_WaveUpload);
+	addfunc(m, t, 1, "UploadWave", 5, 0, 0, ea2_UploadWave);
+	addfunc(m, t, 1, "RenderWave", 6, 0, 1, ea2_RenderWave);
 
 	/* Utilities */
 	eel_export_cfunction(m, 1, "F2P", 1, 0, 0, ea2_F2P);
